f4fbdfc3396621db0e420053af08a06baa0b9355
Amit <amit.sites@gmail.com>
2022-05-26 23:37:05 +0200

    Rebuilt js; Added bin/build

--- docs/assets/javascripts/main.js
+++ docs/assets/javascripts/main.js
@@ -2984,7 +2984,6 @@
     constructor() {
       document.querySelectorAll(".specialties-toggle-btn").forEach((btn) => {
         btn.addEventListener("click", () => {
-          window.amit = btn;
           const classList = btn.querySelector("svg").classList;
           classList.toggle("rotate-0");
           classList.toggle("-rotate-180");
