574f1badf3a1ff59f070b486c6223dcdf460ca83
Amit <amit.sites@gmail.com>
2022-05-26 23:21:45 +0200

    Removed js debug line

--- src/javascripts/specialties.js
+++ src/javascripts/specialties.js
@@ -2,7 +2,6 @@ export default class Specialties {
   constructor() {
     document.querySelectorAll('.specialties-toggle-btn').forEach(btn => {
       btn.addEventListener('click', () => {
-        window.amit = btn;
         const classList = btn.querySelector('svg').classList
         classList.toggle('rotate-0')
         classList.toggle('-rotate-180')
