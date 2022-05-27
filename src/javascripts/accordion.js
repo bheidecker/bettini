214873449548e8c3ce97aef7baf85c654355914f
Amit <amit.sites@gmail.com>
2022-05-27 22:17:15 +0200

    Fixed hit area on the accordion

--- src/javascripts/accordion.js
+++ src/javascripts/accordion.js
@@ -1,12 +1,12 @@
 export default class Accordion {
   constructor() {
-    document.querySelectorAll('.accordion-toggle-btn').forEach(btn => {
-      btn.addEventListener('click', () => {
-        const classList = btn.querySelector('svg').classList
+    document.querySelectorAll('.accordion-element dt').forEach(row => {
+      row.addEventListener('click', () => {
+        const classList = row.querySelector('svg').classList
         classList.toggle('rotate-0')
         classList.toggle('-rotate-180')
 
-        btn.closest('.accordion-element').querySelector('dd').classList.toggle('hidden')
+        row.closest('.accordion-element').querySelector('dd').classList.toggle('hidden')
       })
     })
   }
